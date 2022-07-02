9cf39e6294af13191b4e1ab365b43a16ba74b3bf
Vaishnavi <pranjal.parge@indicchain.com>
2022-07-02 12:01:58 +0530

    localstorage form

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Registeration from './component/Registeration'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="outer">
+        <div className="inner">
+          <Registeration />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
